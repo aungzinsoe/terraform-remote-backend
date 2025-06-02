426ae3d23ce705b7dd3406f5a10d3c58aa09228a
vagrant <vagrant@hellocloud-native-box>
2025-06-02 10:14:39 +0000

    modified output

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "azs-master-s3-bucket-name" {
-  value = data.aws_s3_bucket.aws_azs_master_tfstate.id
+  value = resource.aws_s3_bucket.aws_azs_master_tfstate.id
 }
 
 output "azs-master-dynamodb_name" {
-  value = data.aws_dynamodb_table.terraform_locks.id
+  value = resource.aws_dynamodb_table.terraform_locks.id
 }
